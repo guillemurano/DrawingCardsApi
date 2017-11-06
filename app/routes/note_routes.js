0f87fbb9f54ecff72d28b5ab7d011576e853f43a
guillemurano <guillermo.murano@gmail.com>
2017-11-06 18:37:55 -0300

    Added route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,18 +1,33 @@
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
-    app.get('/notes/:id', (req, res) => {
+    app.get('/card/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
-        db.collection('notes').findOne(details, (err, item) => {
+        db.collection('cards').findOne(details, (err, item) => {
             if (err) res.send({'error':'An error has occurred'});
             else res.send(item);
         });
     });
 
-    app.post("/notes", (req, res) => {
-        const note = { text: req.body.body, title: req.body.title };
-        db.collection('notes').insert(note, (err, result) => {
+    app.get('/random', (req, res) => { 
+        const details = { '_id': new ObjectID(id) };
+        db.collection('cards').find(details, (err, item) => {
+            if (err) res.send({'error':'An error has occurred'});
+            else res.send(item);
+        });
+    });
+
+    app.post("/card", (req, res) => {
+        const body = req.body;
+        const card = { 
+            place: body.place, 
+            object: body.object,
+            action: body.action,
+            difficulty: body.difficulty,
+            allPlay: body.allPlay
+        };
+        db.collection('cards').insert(card, (err, result) => {
             if (err) { 
                 res.send({ 'error': 'An error has occurred' }); 
             } else {
